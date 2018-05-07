a13df94ba92752de2d55cb5de508f8dc9a28d676
dh <dylanhardy@pm.me>
2018-05-07 01:50:29 -0400

    rude hello

---
+++ helloworld/program.py
@@ -0,0 +1,9 @@
+print('--------------------')
+print('STUPID ASS HELLO APP')
+print('--------------------')
+
+user_name = input('What is your name? ')
+
+rude_greeting = 'that is a dumb ass name, ' + user_name
+
+print(rude_greeting)
