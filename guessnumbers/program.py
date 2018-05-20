e71d33e1ce843b372efffe890b693070a6d32fd6
dh <dylanhardy@pm.me>
2018-05-20 05:11:29 -0400

    def?

--- guessnumbers/program.py
+++ guessnumbers/program.py
@@ -1,25 +1,32 @@
 import random
 
-number = random.randint(1, 100)
 
-gamestart = input("do you want to play a game?: ")
+def start():
+    answer = input("do you want to play a game?: ")
+    if answer == "yes":
+        print("good choice")
+    elif answer == "no":
+        print("too bad, here we go")
+    else:
+        print("ok well here we go anyway")
 
-if gamestart == "yes":
-    print("good choice")
-elif gamestart == "no":
-    print("too bad, here we go")
-else:
-    print("ok well here we go anyway")
 
-guess = 101
-
-while guess != number:
+def game():
+    number = random.randint(1, 100)
+    guess = 101
+    while guess != number:
         game = input("\ni'm thinking of a number between 1 and 100\nplease take a guess: ")
         guess = int(game)
         if guess > number:
             print("{} is too high".format(guess))
         if guess < number:
             print("{} is too low".format(guess))
-else:
-    print("wow, {} is right! you won, idiot.")
+    else:
+        print("wow, {} is right! you won, idiot.".format(guess))
 
+
+def main():
+    start()
+    game()
+
+main()
